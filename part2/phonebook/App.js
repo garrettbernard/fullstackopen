e2bad09de53066ecaa6639ea97114a10397b84ac
garrettbernard <garrett.bernard@gmail.com>
2021-04-10 17:55:12 -0400

    Add files via upload

--- part2/phonebook/App.js
+++ part2/phonebook/App.js
@@ -1,14 +1,23 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import PersonForm from './components/PersonForm'
 import Filter from './components/Filter'
 import Person from './components/Person'
+import axios from 'axios'
 
 const App = (props) => {
-  const [ persons, setPersons ] = useState(props.staticNames)
+  const [ persons, setPersons ] = useState([])
   const [ newSearch, setNewSearch ] = useState('')
   const [ newName, setNewName ] = useState('')
   const [ newPhone, setNewPhone ] = useState('')
 
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
 
   return (
     <div>
