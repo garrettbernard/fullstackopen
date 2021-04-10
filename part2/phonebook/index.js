3805449027cfe68d4a840135688f627f0103d6ea
garrettbernard <garrett.bernard@gmail.com>
2021-04-10 17:55:53 -0400

    Add files via upload

--- part2/phonebook/index.js
+++ part2/phonebook/index.js
@@ -1,20 +1,8 @@
 import ReactDOM from 'react-dom'
 import App from './App.js'
 
-const staticNames = [
-  {
-    content: 'Arto Hellas',
-    number: '040-1234567',
-    id: 1
-  },
-  {
-    content: 'Ada Lovelace',
-    number: '39-44-5323523',
-    id: 2
-  }
-]
 
 ReactDOM.render(
-  <App staticNames={staticNames} />,
+  <App />,
   document.getElementById('root')
 )
